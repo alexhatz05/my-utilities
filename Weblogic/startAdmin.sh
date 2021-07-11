72884199efab1f36484064662c88e9e05c2fe6eb
Alexia Chatzikonstantinou <alexiahatzi05@gmail.com>
2021-07-11 16:25:21 +0300

    Add bash scripts for starting Weblogic servers

---
+++ Weblogic/startAdmin.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sh /data/weblogic12_2/Oracle/Middleware/Oracle_Home/user_projects/domains/dev_domain12/startWebLogic.sh
---
+++ Weblogic/startWLManaged.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sh /data/weblogic12_2/Oracle/Middleware/Oracle_Home/user_projects/domains/dev_domain12/bin/startManagedWebLogic.sh WL_MANAGED_NAME http://127.0.0.1:7001
